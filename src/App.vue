5778e818e900910f9c35e461803d125fcf7106eb
Eric Lifszyc <lifszyc.eric@gmail.com>
2020-05-23 21:15:53 -0300

    Add building component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Building :floors="floors"></Building>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Building from './components/Building.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Building
+  },
+  data() {
+    return {
+      floors: 7,
+    };
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      height: 100%;
+    }
+  }
 </style>
---
+++ src/components/Building.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="building">
+    <div class="building__floor" v-for="floor in floorsArray" v-bind:key="floor">
+      {{floor ? floor : 'PB'}}
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Building',
+  props: {
+    floors: Number,
+  },
+  computed: {
+    floorsArray() {
+      return Array.from(Array(this.floors).keys()).reverse()
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .building {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    &__floor {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      background: #a4b0be;
+      justify-content: center;
+      border-top: 1px solid black;
+      border-bottom: 1px solid black;
+    }
+  }
+</style>
