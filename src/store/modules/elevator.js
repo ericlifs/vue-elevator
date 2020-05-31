62ae1db1af51d698cd679c03545cba5fa4718bc4
Eric Lifszyc <lifszyc.eric@gmail.com>
2020-05-31 11:37:11 -0300

    Add first version of waiting input timeout

--- src/store/modules/elevator.js
+++ src/store/modules/elevator.js
@@ -9,14 +9,14 @@ const state = {
 const getters = {
   currentFloor: state => state.currentFloor,
   isQueueEmpty: state => state.queue.length === 0,
-  //firstFromQueue: state => state.queue[0],
+  firstFromQueue: state => state.queue[0] || 0,
   status: state => state.status,
   queue: state => state.queue
 }
 
 const actions = {
   goToFloor: ({ commit, getters }, floor) => {
-    if (getters.status !== ELEVATOR_STATUS.ON_TRAVEL) {
+    if (getters.status !== ELEVATOR_STATUS.ON_TRAVEL && getters.status !== ELEVATOR_STATUS.WAITING_FOR_INPUT) {
       commit('goToFloor', floor)
       commit('setStatus', ELEVATOR_STATUS.ON_TRAVEL)
       return;
@@ -26,9 +26,17 @@ const actions = {
       commit('addToQueue', floor)
     }
   },
-  onTravelEnded: ({ commit }, floor) => {
+  onTravelEnded: ({ commit, getters, dispatch }, floor) => {
     const nextStatus = floor ? ELEVATOR_STATUS.WAITING_FOR_INPUT : ELEVATOR_STATUS.INITIAL
     commit('setStatus', nextStatus)
+
+    setTimeout(() => {
+      const nextFloor = getters.firstFromQueue
+      const nextStatus = nextFloor ? ELEVATOR_STATUS.ON_TRAVEL : ELEVATOR_STATUS.INITIAL
+
+      commit('setStatus', nextStatus)
+      dispatch('goToFloor', nextFloor)
+    }, 5000)
   }
 }
 
