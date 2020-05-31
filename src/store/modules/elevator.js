a9f2fb01e017d7e90f00a7e1bb538fcdc2ce499a
Eric Lifszyc <lifszyc.eric@gmail.com>
2020-05-31 11:23:40 -0300

    Add queuing floors on request

--- src/store/modules/elevator.js
+++ src/store/modules/elevator.js
@@ -9,15 +9,22 @@ const state = {
 const getters = {
   currentFloor: state => state.currentFloor,
   isQueueEmpty: state => state.queue.length === 0,
-  firstFromQueue: state => state.queue[0],
+  //firstFromQueue: state => state.queue[0],
   status: state => state.status,
   queue: state => state.queue
 }
 
 const actions = {
-  goToFloor: ({ commit }, floor) => {
-    commit('goToFloor', floor)
-    commit('setStatus', ELEVATOR_STATUS.ON_TRAVEL)
+  goToFloor: ({ commit, getters }, floor) => {
+    if (getters.status !== ELEVATOR_STATUS.ON_TRAVEL) {
+      commit('goToFloor', floor)
+      commit('setStatus', ELEVATOR_STATUS.ON_TRAVEL)
+      return;
+    }
+
+    if (!getters.queue.includes(floor)) {
+      commit('addToQueue', floor)
+    }
   },
   onTravelEnded: ({ commit }, floor) => {
     const nextStatus = floor ? ELEVATOR_STATUS.WAITING_FOR_INPUT : ELEVATOR_STATUS.INITIAL
@@ -26,9 +33,10 @@ const actions = {
 }
 
 const mutations = {
-  removeFirstFromQueue: (state) => state.queue.shift(),
+  //removeFirstFromQueue: (state) => state.queue.shift(),
   goToFloor: (state, floor) => state.currentFloor = floor,
-  setStatus: (state, status) => state.status = status
+  setStatus: (state, status) => state.status = status,
+  addToQueue: (state, floor) => state.queue = [...state.queue, floor]
 }
 
 export default {
